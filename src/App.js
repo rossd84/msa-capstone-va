94eaf07ddf2803a9032ec1efb3a008fbdfe980af
Darby Ross <darby.e.ross@gmail.com>
2020-05-17 14:07:22 -0500

    add functionality to tabs
    correct radio button sizes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { Header } from './components/Header'
 import Home from './components/Main'
 import { Lists } from './components/Lists'
+import { Calendar } from './components/Calendar'
 
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentTab: 'home'
+      currentTab: 'home',
+      listItems: ['Sample 1', 'Sample 2', 'Sample 3', 'Sample 4', 'Sample 5']
     }
   }
 
@@ -21,14 +23,27 @@ class App extends Component {
   }
 
 render() {
+  let currentPage = this.state.currentTab;
+  console.log('render currentPage', currentPage)
   return (
   <>
     <Header 
       changeTab={this.onChangeTab.bind(this)}
       initialTab={this.state.currentTab} 
     />
-    <Home />
-    <Lists />
+    { currentPage.tab === 'lists' 
+      ? <Lists 
+          listItems={this.state.listItems}
+        />
+      : currentPage.tab === 'calendar'
+      ? <Calendar />
+      : <Home />
+    }
+
+    {/* <Home />
+    <Lists 
+      listItems={this.state.listItems}
+    /> */}
   </>
   )
 }
--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Collapse, Form, Row, Col } from 'react-bootstrap';
+import { Button, Collapse, Form, FormControl, Col, InputGroup } from 'react-bootstrap';
 
 export function AddItem() {
   const [open, setOpen] = useState(false);
@@ -14,28 +14,33 @@ export function AddItem() {
       >Add Item</Button>
       <Collapse in={open}>
         <Form>
-          <Form.Group as={Row}>
+          <Form.Group>
             <Form.Row width='100%'>
-              <Col>
+              <Col className="col-sm-2">
                 <Form.Label column>
                   Item
                 </Form.Label>
               </Col>              
-              <Col className="col-sm-8">
+              <Col className="col-sm-10">
                 <Form.Control type="text" placeholder="e.g. Go to grocery store" />
               </Col>
             </Form.Row>
             <Form.Row>
-              <Col>
+              <Col className="col-sm-2">
                 <Form.Label column>
                   Due By
                 </Form.Label>
               </Col>
-              <Col>
+              <Col className="col-sm-2">
                 <Form.Control type="date" />
+              </Col>
+              <Col className="col-sm-2">
+                <Form.Control type="time" />
               </Col>
             </Form.Row>
           </Form.Group>
+          <Form.Check inline label="Personal" type='radio' id='personal' />
+          <Form.Check inline label="Work" type='radio' id='work' />
           <Button 
             type="submit"
             block
